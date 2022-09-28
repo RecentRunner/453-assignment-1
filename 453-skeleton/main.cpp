68444456cf48e54740c386f7bb0a7e6f6eb47049
Harkamal Randhawa <harkamalsr@gmail.com>
2022-09-28 12:33:13 -0600

    Added equilateral triangle and beginnings of sierpinsky

--- 453-skeleton/main.cpp
+++ 453-skeleton/main.cpp
@@ -1,7 +1,10 @@
+#include <cmath>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <glm/ext/scalar_constants.hpp>
 #include <glm/fwd.hpp>
 #include <iostream>
+#include <math.h>
 #include "Geometry.h"
 #include "GLDebug.h"
 #include "Log.h"
@@ -17,8 +20,16 @@ class MyCallbacks : public CallbackInterface {
 public:
 	MyCallbacks(ShaderProgram& shader) : shader(shader) {}
     
+    // Press 'R' to refresh/recompile shader
 	virtual void keyCallback(int key, int scancode, int action, int mods) {
 		if (key == GLFW_KEY_R && action == GLFW_PRESS) {
+			shader.recompile();
+		}
+	}
+
+    // Press 'R' to refresh/recompile shader
+	virtual void keyCallback2(int key, int scancode, int action, int mods) {
+		if (key == GLFW_KEY_RIGHT && action == GLFW_PRESS) {
 			shader.recompile();
 		}
 	}
@@ -27,12 +38,61 @@ private:
 	ShaderProgram& shader;
 };
 
-void sierpinski(int n){
+GPU_Geometry drawTriangle(vec3 v1,vec3 v2,vec3 v3,vec3 color){
+ 	// GEOMETRY
+	CPU_Geometry cpuGeom;
+	GPU_Geometry gpuGeom;
+
+    cpuGeom.verts.push_back(v1);
+    cpuGeom.verts.push_back(v2);
+    cpuGeom.verts.push_back(v3);
+
+    // colours (these should be in linear space)
+	cpuGeom.cols.push_back(color);
+	cpuGeom.cols.push_back(color);
+	cpuGeom.cols.push_back(color);
+
+	gpuGeom.setVerts(cpuGeom.verts); // Send vertices to GPU 
+	gpuGeom.setCols(cpuGeom.cols); // Send colors to GPU
+   
+    return gpuGeom;
+}
+
+GPU_Geometry sierpinsky(int n){
+    double dwn_trns = 0.2;
     
+    // vertices
+	// Bottom left
+    vec3 v1 = vec3(cos(M_PI/2), sin(M_PI/2)-dwn_trns, 0.f);
+    
+    // Bottom right
+    vec3 v2 = vec3(cos((4*M_PI)/3+(M_PI/2)), sin((4*M_PI)/3+(M_PI/2))-dwn_trns, 0.f);
+    
+    // Top
+    vec3 v3 = vec3(cos(((2*M_PI)/3)+(M_PI/2)), sin(((2*M_PI)/3)+(M_PI/2))-dwn_trns, 0.f);
+
+    // Color
+    vec3 color = vec3(0.0f,1.0f,0.0f);
+
+    GPU_Geometry gpuGeom = drawTriangle(v1,v2,v3,color);
+
+    return gpuGeom;
+}
+
+void kochSnowflake(int n){
+} 
+
+void uniformTriangleMassCenter(int n){
+}
+
+void dragonCurve(int n){
 }
 
 void drawTriangle(vec3 a, vec3 b, vec3 c){
 
+}
+
+void drawLine(vec3 a, vec3 b){
 }
 
 int main() {
@@ -40,7 +100,7 @@ int main() {
 
 	// WINDOW
 	glfwInit();
-	Window window(800, 800, "Assignment 1 - Harkamal Randhawa"); // can set callbacks at construction if desired
+	Window window(800, 800, "CPSC 453"); // can set callbacks at construction if desired
 
 	GLDebug::enable();
 
@@ -50,33 +110,18 @@ int main() {
 	// CALLBACKS
 	window.setCallbacks(std::make_shared<MyCallbacks>(shader)); // can also update callbacks to new ones
 
-	// GEOMETRY
-	CPU_Geometry cpuGeom;
-	GPU_Geometry gpuGeom;
+    GPU_Geometry sierpinksi = sierpinsky(0);
 
-	// vertices
-	cpuGeom.verts.push_back(vec3(-0.5f, -0.5f, 0.f));
-	cpuGeom.verts.push_back(vec3(0.5f, -0.5f, 0.f));
-	cpuGeom.verts.push_back(vec3(0.f, 0.5f, 0.f));
-	
-    // colours (these should be in linear space)
-	cpuGeom.cols.push_back(vec3(1.f, 0.f, 0.f));
-	cpuGeom.cols.push_back(vec3(0.f, 1.f, 0.f));
-	cpuGeom.cols.push_back(vec3(0.f, 0.f, 1.f));
-
-	gpuGeom.setVerts(cpuGeom.verts); // Send vertices to GPU 
-	gpuGeom.setCols(cpuGeom.cols); // Send colors to GPU
-
-	// RENDER LOOP
+    // RENDER LOOP
 	while (!window.shouldClose()) {
 		glfwPollEvents(); // Refresh screen
 
 		shader.use();
-		gpuGeom.bind(); // Bind gpu geometry to OpenGL for use
+		sierpinksi.bind(); // Bind gpu geometry to OpenGL for use
 
 		glEnable(GL_FRAMEBUFFER_SRGB); // Enable framebuffer
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clear color and depth buffer
-		glDrawArrays(GL_LINE_LOOP, 0, 3);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 		glDisable(GL_FRAMEBUFFER_SRGB); // disable sRGB for things like imgui
 
 		window.swapBuffers(); // Swapping the double buffer
